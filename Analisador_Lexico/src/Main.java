8ccb01215ee88cd91ea3f6bcd7f12775c851bc8c
Indra Araujo <indra.araujo.santos@gmail.com>
2021-08-27 22:10:57 -0300

    Add files via upload

---
+++ Analisador_Lexico/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Reader fileReader = new Reader("teste.txt");
+        fileReader.readFile();
+    }
+}
---
+++ Analisador_Lexico/src/Reader.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileReader;
+
+
+public class Reader {
+    File file;
+    String fileName;
+   public Reader(String filePath){
+        fileName = filePath;
+        file = new File(fileName);
+   } 
+   
+   public void readFile(){
+        try (FileReader fr = new FileReader(file))
+        {
+            int content;
+            while ((content = fr.read()) != -1) {
+                char character = (char) content;
+                //chamar função passando o caracter
+                System.out.print(character);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+   }
+   
+}
