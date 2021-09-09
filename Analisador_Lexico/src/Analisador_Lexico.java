2f6dfb853dee3f5637fe13c67345e6d4bbd24d6e
indraAraujo <indra.araujo.santos@gmail.com>
2021-09-09 15:04:14 -0300

    mostrar resultado no text area

--- Analisador_Lexico/src/Analisador_Lexico.java
+++ Analisador_Lexico/src/Analisador_Lexico.java
@@ -1,13 +1,9 @@
 
+
 import java.io.File;
 import java.util.ArrayList;
 import javax.swing.JFileChooser;
-import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author indra
- */
 public class Analisador_Lexico extends javax.swing.JFrame {
 
      ArrayList<String> readCharacters = new ArrayList<>();
@@ -17,34 +13,19 @@ public class Analisador_Lexico extends javax.swing.JFrame {
     }
 
   
-                          
+                             
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        result_table = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         get_file_button = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        result_area = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-
-        jScrollPane1.setBackground(new java.awt.Color(255, 255, 255));
-
-        result_table.setFont(new java.awt.Font("Bahnschrift", 0, 11)); // NOI18N
-        result_table.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-
-            },
-            new String [] {
-                "Tipo", "Token"
-            }
-        ));
-        result_table.setGridColor(new java.awt.Color(255, 255, 255));
-        result_table.setSelectionBackground(new java.awt.Color(228, 255, 246));
-        jScrollPane1.setViewportView(result_table);
 
         jLabel1.setFont(new java.awt.Font("Bahnschrift", 0, 11)); // NOI18N
         jLabel1.setText("Resultado da análise léxica");
@@ -62,38 +43,43 @@ public class Analisador_Lexico extends javax.swing.JFrame {
             }
         });
 
+        result_area.setEditable(false);
+        result_area.setColumns(20);
+        result_area.setRows(5);
+        jScrollPane2.setViewportView(result_area);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(78, 78, 78)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(162, 162, 162)
-                        .addComponent(jLabel1)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(55, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel2)
-                .addGap(36, 36, 36)
+                .addGap(33, 33, 33)
                 .addComponent(get_file_button)
-                .addGap(56, 56, 56))
+                .addGap(55, 55, 55))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jLabel1)
+                .addGap(41, 41, 41)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(53, 53, 53)
+                .addGap(43, 43, 43)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(get_file_button)
                     .addComponent(jLabel2))
-                .addGap(81, 81, 81)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(65, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(83, 83, 83)
+                        .addComponent(jLabel1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(57, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -104,16 +90,14 @@ public class Analisador_Lexico extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>                        
 
     private void get_file_buttonActionPerformed(java.awt.event.ActionEvent evt) {                                                
-       //-------- LIDAR COM O ARQUIVO ------------
+        //-------- LIDAR COM O ARQUIVO ------------
         String fileName ="";
         JFileChooser chooser = new JFileChooser(".");
         int res;
@@ -132,19 +116,14 @@ public class Analisador_Lexico extends javax.swing.JFrame {
            }
         
           //COLOCAR O RESULTADO NA TABELA
+          //result = automata.getResult(); TODO! PASSAR O RESULTADO DA CLASSE AUTOMATA;
+
           result.add(new Token("Palavra Reservada", "nil"));
           result.add(new Token("Palavra Reservada", "car"));
           result.add(new Token("Operador", "leq"));
           result.add(new Token("Identificador", "Ab10_Bah"));
-          //result = automata.getResult(); TODO! PASSAR O RESULTADO DA CLASSE AUTOMATA;
-          String[][] result_matrix = {
-              {"Palavra Reservada", "nil"}
-          };
-         
-          DefaultTableModel dtm_result = (DefaultTableModel)result_table.getModel();
-          for(int i=0; i<result_matrix.length; i++){
-              dtm_result.addRow(result_matrix[i]);
-          }
+          
+          result_area.setText(result.toString());
 
           //SALVAR O RESULTADO EM UM ARQUIVO
           Reader fileReader = new Reader(fileName);
@@ -167,7 +146,7 @@ public class Analisador_Lexico extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable result_table;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea result_area;
     // End of variables declaration                   
 }
