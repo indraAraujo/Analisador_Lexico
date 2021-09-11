403843c161e2925f4bb68d81ec2e58f40f1c044c
Tomas Benfica <tomas_r_benfica@hotmail.com>
2021-09-11 20:59:05 -0300

    Modificações para versão funcional do analisador léxico. A GUI não foi incorporada.

---
+++ Analisador_Lexico/src/scheme.scm
@@ -0,0 +1,10 @@
+defun define let lambda
++ - * * * * * - - - - -
+car cdr
+defun define let lambda
+list
+defun define let lambda
+atom cond case if T nil
+car cdr
++ * define - vjfddfn / < leq > geq neq eq =
+variable1 v!@#87 i9789785 var6754654
