80b963ba758f642b5b2e1d883edcf7c4816a54aa
indraAraujo <indra.araujo.santos@gmail.com>
2021-10-06 23:20:58 -0300

    pilha

--- Analisador_Lexico/src/Analisador_Sintatico/Main.java
+++ Analisador_Lexico/src/Analisador_Sintatico/Main.java
@@ -1,8 +1,23 @@
 package Analisador_Sintatico;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String args[]) {
-        
+        Stack pilha = new Stack();
+
+        pilha.push("S");
+        System.out.println(pilha.peek());
+
+        pilha.push("(defun N (L) C) S");
+        System.out.println(pilha.peek());
+
+        pilha.push("identificador");
+        System.out.println(pilha.peek());
+        System.out.println("Item de valor " + pilha.pop() + " retirado da pilha");
+        System.out.println("Item atual no topo: " + pilha.peek());
+        System.out.println("Item de valor " + pilha.pop() + " retirado da pilha");
+        System.out.println("Item atual no topo: " + pilha.peek()); 
     }
 
 }
